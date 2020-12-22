7c85dc2eb8f6b878f92a1402b4d59272481680bc
therez.causing <therez.magnolia@gmail.com>
2020-12-22 21:33:44 +0800

    removed unused methods

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,13 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import sanityClient from "../client";
-import imageUrlBuilder from "@sanity/image-url";
-import BlockContent from "@sanity/block-content-to-react";
-
-const builder = imageUrlBuilder(sanityClient);
-function urlFor(source) {
-  return builder.image(source);
-}
 
 export default function SinglePost() {
   const [singlePost, setSinglePost] = useState(null);
